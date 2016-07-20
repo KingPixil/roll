c0e4b1102a27b46675893d7844307e76c854f0f6
Gopal Shah <simplecooldude1@gmail.com>
2016-07-19 21:05:37 -0700

    gradient background

--- roll/ViewController.swift
+++ roll/ViewController.swift
@@ -9,9 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let gradientLayer = CAGradientLayer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // background gradient
+        self.view.backgroundColor = UIColor.blueColor()
+        gradientLayer.frame = self.view.bounds
+        let color1 = UIColor(red: 0.20, green: 0.56, blue: 0.31, alpha: 1.0).CGColor as CGColorRef
+        let color2 = UIColor(red: 0.34, green: 0.71, blue: 0.83, alpha: 1.0).CGColor as CGColorRef
+        gradientLayer.colors = [color1, color2]
+        gradientLayer.locations = [0.0, 1.0]
+        self.view.layer.addSublayer(gradientLayer)
         
     }
 
