17b675e1a6f09252d9d8e73e4f455bbfc546071d
Gopal Shah <simplecooldude1@gmail.com>
2016-07-19 21:50:20 -0700

    make gradient at index 0

--- roll/MainTableViewController.swift
+++ roll/MainTableViewController.swift
@@ -17,13 +17,12 @@ class MainTableViewController: UITableViewController {
         super.viewDidLoad()
         
         // background gradient
-        self.view.backgroundColor = UIColor.blueColor()
         gradientLayer.frame = self.view.bounds
         let color1 = UIColor(red: 0.20, green: 0.56, blue: 0.31, alpha: 1.0).CGColor as CGColorRef
         let color2 = UIColor(red: 0.34, green: 0.71, blue: 0.83, alpha: 1.0).CGColor as CGColorRef
         gradientLayer.colors = [color1, color2]
         gradientLayer.locations = [0.0, 1.0]
-        self.view.layer.addSublayer(gradientLayer)
+        self.view.layer.insertSublayer(gradientLayer, atIndex:0)
         
     }
     
